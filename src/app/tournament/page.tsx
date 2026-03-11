05add99e98572f79d26b2ddd8b6b8500e476730b
Dinon Isaig <dinonbonagua@gmail.com>
2026-03-12 02:44:56 +0800

    feat(tournament): apply importing of page to route

--- src/app/tournament/page.tsx
+++ src/app/tournament/page.tsx
@@ -1,9 +1,13 @@
 import PageBackground from "@/components/page-background";
+import Tournament from "@/features/tournament";
+
 
 export default function TournamentPage() {
     return (
         <PageBackground>
             {/* Tournament page content goes here */}
+            <Tournament/>
         </PageBackground>
+        
     );
 }
